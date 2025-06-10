b9c4785767a2465c8bf92e731a8e4fa8200b9010
leonid <leniar.074@gmail.com>
2025-06-10 23:44:31 +0500

    События 🎆

---
+++ js/lesson16.js
@@ -0,0 +1,40 @@
+
+// click
+
+document.getElementById('p1').addEventListener('click', function() {
+    document.getElementById('button-click').classList.add('red')
+})
+
+// mouseover
+
+const p2 = document.getElementById('p2')
+p2.addEventListener('mouseover', function() {
+    p2.style.fontSize = "1.1em"
+    p2.style.color = "gray"
+})
+
+p2.addEventListener('mouseout', function() {
+    p2.style.fontSize = ""
+    p2.style.color = ""
+})
+
+// keyup
+
+const keyUp = document.getElementById('input-p3')
+keyUp.addEventListener('keyup', function(e) {
+    console.log(e.keyCode)
+})
+
+// submit
+
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault()
+    alert('Успешно отправлено!')
+})
+
+// toggle
+
+const buttonTheme = document.getElementById('button-theme')
+buttonTheme.addEventListener('click', function() {
+    document.getElementById('body').classList.toggle('night-theme')
+})
