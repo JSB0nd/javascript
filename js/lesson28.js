33c3057c7fabe37bba2abdef49fb9d44b97f3b5e
leonid <leniar.074@gmail.com>
2025-07-19 17:59:07 +0500

    Promise 🤝🏻 обновлен

--- js/lesson28.js
+++ js/lesson28.js
@@ -1,21 +1,20 @@
 // 1. Напиши функцию `getUserData`, которая возвращает промис с данными пользователя через 2 секунды.
 // Затем создай цепочку промисов, которая обрабатывает эти данные и выводит результат в консоль;
 
+const getUserData= new Promise((resolve, reject) => {
+    setTimeout(function () {
+        user = {
+            name: "John",
+            age: 25,
+        };
 
+        console.log('Requesting user data...');
+        resolve(user);
+    }
+    , 2000);
+}) //
 
-const promiseUserData = new Promise((resolve, reject) => {
-    setTimeout(function getUserData(){
-        console.log('Preparing user data...');
-            userData = {
-                name: "John",
-                age: 25,
-            }
-            resolve(userData) // возвращаем объект userData
-        }
-        , 2000);
-}); // Preparing user data...
-
-promiseUserData.then((data)=> {
+getUserData.then((data)=> {
         const p = new Promise((resolve, reject) => {
             setTimeout(function () {
                     console.log('Getting user data...');
@@ -26,7 +25,7 @@ promiseUserData.then((data)=> {
 
     p.then((data) => {
             setTimeout(function () {
-                    console.log('Promise resolved ', data);
+                    console.log('Data received: ', data);
                 }
                 , 1000);
         }
@@ -34,14 +33,14 @@ promiseUserData.then((data)=> {
 
     p.finally(() => {
         setTimeout(function () {
-            console.log('User Data have gotten successfully.');
+            console.log('User Data received successfully.');
         }, 1000)
 
     })// Promise resolved {name: 'John', age: 25}
 })
 
 
-// 2. Напиши две функции, каждая из которых возвращает промис с данными через 3 и 5 (5 и 7) секунд соответственно.
+// 2. Напиши две функции, каждая из которых возвращает промис с данными через 3 и 5 секунд соответственно.
 // Используй такой метод промисов, чтобы дождаться выполнения обоих промисов и вывести результаты в консоль;
 
 const fetchData = new Promise((resolve, reject) => {
@@ -51,7 +50,7 @@ const fetchData = new Promise((resolve, reject) => {
         }
         console.log('Fetching data...');
         resolve(data);
-    }, 5000)
+    }, 3000)
 })
 
 const fetchData2 = new Promise((resolve, reject) => {
@@ -61,7 +60,7 @@ const fetchData2 = new Promise((resolve, reject) => {
         }
         console.log('Fetching data2...');
         resolve(data2);
-    }, 6000)
+    }, 5000)
 })
 
 Promise.all([fetchData, fetchData2])
@@ -79,19 +78,19 @@ Promise.all([fetchData, fetchData2])
 const fetchData3 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve('Данные загружены Data3');
-    }, (Math.ceil(Math.random() * 5) + 7) * 1000); // получаем рандомное число от от 1 до 5 секунд (+7)
+    }, (Math.ceil(Math.random() * 5)) * 1000); // получаем рандомное число от 1 до 5 секунд
 });
 
 const fetchData4 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve('Данные загружены Data4');
-    }, (Math.ceil(Math.random() * 5) + 7) * 1000); // получаем рандомное число от от 1 до 5 секунд (+7)
+    }, (Math.ceil(Math.random() * 5)) * 1000); // получаем рандомное число от 1 до 5 секунд
 });
 
 const fetchData5 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve('Данные загружены Data5');
-    }, (Math.ceil(Math.random() * 5) + 7) * 1000); // получаем рандомное число от от 1 до 5 секунд (+7)
+    }, (Math.ceil(Math.random() * 5)) * 1000); // получаем рандомное число от 1 до 5 секунд
 });
 
 Promise.race([fetchData3, fetchData4, fetchData5])
