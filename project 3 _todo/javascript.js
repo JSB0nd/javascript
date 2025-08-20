e60e753a5172c253ad26b42e26ca5bb865446f0d
Leonid <leniar.074@gmail.com>
2025-08-20 17:57:21 +0500

    To-Do List📝 task options

--- project 3 _todo/javascript.js
+++ project 3 _todo/javascript.js
@@ -2,16 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskList = document.getElementById('task-list');
     const inputField = document.getElementById('input');
     const submitForm = document.querySelector('form');
-
-// загрузка из LocalStorage
+    const sortTaskUnsort = document.getElementById('unsorted');
+    const sortTaskActive = document.getElementById('active');
+    const sortTaskComplete = document.getElementById('completed');
+    let filter = 'all';
+    
+    // загрузка из LocalStorage
     let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-// сохранение в LocalStorage
+    // сохранение в LocalStorage
     function saveTasks() {
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 
-// отрисовка задач
+    // отрисовка задач
     function renderTasks() {
         taskList.innerHTML = '';
         tasks.forEach((task, index) => {
@@ -31,50 +35,74 @@ document.addEventListener('DOMContentLoaded', () => {
             deleteBtn.addEventListener('click', () => deleteTask(index));
 
             const timer = li.querySelector('.task-timer');
-            timer.addEventListener('click', ()=> timerTask(task.text))
+            timer.addEventListener('click', () => timerTask(task.text))
 
             taskList.appendChild(li);
         });
     }
 
-// добавление задачи
+    // фильтрация задач
+    function getFilteredTask() {
+        switch (filter) {
+            case 'active':
+                return tasks.filter(task => !task.completed);
+            case 'completed':
+                return tasks.filter(task => task.completed);
+            default:
+                return tasks;
+        }
+    }
+
+    function renderTasks() {
+        taskList.innerHTML = '';
+        const filteredTasks = getFilteredTask();
+
+        filteredTasks.forEach((task, index) => {
+            const realIndex = tasks.findIndex(t => t === task);
+
+            const li = document.createElement('li');
+            li.className = `task-item ${task.completed ? ' completed' : ''}`;
+
+            li.innerHTML = `
+                <input type="checkbox" ${task.completed ? 'checked' : ''}>
+                <span class="task-text">${task.text}</span>
+                <button class="task-timer">⏰</button>
+                <button class="delete-btn button">Удалить</button>
+    `;
+
+            const checkbox = li.querySelector('input[type="checkbox"]');
+            checkbox.addEventListener('change', () => toggleTask(index));
+
+            const deleteBtn = li.querySelector(".delete-btn");
+            deleteBtn.addEventListener('click', () => deleteTask(index));
+
+            const timer = li.querySelector('.task-timer');
+            timer.addEventListener('click', () => timerTask(task.text))
+
+            taskList.appendChild(li);
+        });
+    }
+
+    // добавление задачи
     function addTask(text) {
-        tasks.push({text: text, completed: false});
+        tasks.push({ text: text, completed: false });
         saveTasks();
         renderTasks();
     }
 
-// проверка чекбокса
+    // проверка чекбокса
     function toggleTask(index) {
         tasks[index].completed = !tasks[index].completed;
         saveTasks();
         renderTasks();
     }
 
-// удаление задачи
+    // удаление задачи
     function deleteTask(index) {
         tasks.splice(index, 1);
         saveTasks();
         renderTasks();
     }
-
-//  слушатель инпута
-    submitForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const text = inputField.value.trim();
-        if (text) {
-            addTask(text);
-            inputField.value = '';
-        } else {
-            alert('Задача не может быть пустой!');
-        }
-    });
-
-    // timer.addEventListener('click', () => {
-    //     prompt('Сколько секунд таймера?', timerStamp);
-    //     let timerId = setTimeout(() => alert(`${text}`), timerStamp)
-    //     clearTimeout(timerId)
-    // });
 
     function timerTask(text) {
         const timerStampString = prompt('Сколько секунд таймера?', '5');
@@ -91,12 +119,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
             setTimeout(() => {
                 alert(`⏰ Время дла задачи: ${text}`);
-                }, timerStamp * 1000);
+            }, timerStamp * 1000);
         } else {
             alert('Пожалуйста, введите положительное число!')
             console.log(`${timerStamp} не является положительным числом`);
         }
     }
+
+
+    //  обработчики событий
+    submitForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const text = inputField.value.trim();
+        if (text) {
+            addTask(text);
+            inputField.value = '';
+        } else {
+            alert('Задача не может быть пустой!');
+        }
+    });
+
+    sortTaskUnsort.addEventListener('click', () => {
+        filter = 'all';
+        renderTasks();
+    });
+
+    sortTaskActive.addEventListener('click', () => {
+        filter = 'active';
+        renderTasks()
+    });
+
+    sortTaskComplete.addEventListener('click', () => {
+        filter = 'completed';
+        renderTasks()
+    });
 
     renderTasks();
 });
